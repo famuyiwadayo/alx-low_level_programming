cbb8d1bf32df8d114dcfacee79b93d2813f90f80
Dayo Famuyiwa <famuyiwadayodaniel@gmail.com>
2023-03-16 16:26:34 -0700

    added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry file
+ * Description - main entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
