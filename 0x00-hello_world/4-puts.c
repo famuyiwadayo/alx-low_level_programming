7a0da938e2bbfadde29d226069c8cc727505cea9
Dayo Famuyiwa <famuyiwadayodaniel@gmail.com>
2023-03-16 15:15:15 -0700

    fixed betty-style compliance issues with the c files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry function
+ * Description - 'This is the main entry point of the main.c file'
+ * @parameter - void
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry function
+ * Description - 'This is main entry point of the 5-printf.c file'
+ * @parameter - void
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
