f261070490c89a120318beb661dc285e77e65115
Dayo Famuyiwa <famuyiwadayodaniel@gmail.com>
2023-03-20 01:50:50 -0700

    added program to print base 10 digits from 0 to 9

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * Description - the main entry point
+ * Return: Always 0 success
+ */
+int main(void)
+{
+	char ascii_a = 'a';
+
+	while (ascii_a <= 'z')
+	{
+		if (ascii_a == 'e' || ascii_a == 'q')
+			continue;
+		putchar(ascii_a);
+		ascii_a++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * Description - this is the main entry point of this program
+ * Returns - always 0 success
+ */
+
+int main(void)
+{
+	unsigned int i = 0;
+	
+	while(i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
