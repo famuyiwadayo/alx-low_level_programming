4624fd482b969cec5be7bf53ba31b44af09ad031
Dayo Famuyiwa <famuyiwadayodaniel@gmail.com>
2023-03-20 03:28:33 -0700

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * Description - This is the main entry point of this program
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	unsigned int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
