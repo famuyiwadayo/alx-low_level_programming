d6045102c2c62e90e5158f70be85d9bfa0f4f7dd
Dayo Famuyiwa <famuyiwadayodaniel@gmail.com>
2023-03-22 13:47:02 -0700

    completed task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * Description: The main entry point for this program
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
